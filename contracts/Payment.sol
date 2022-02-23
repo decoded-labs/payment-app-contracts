d72387f293deef77887a77279c10b4affcf021b9
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2022-02-23 18:52:53 +0200

    Create Payment.sol file

---
+++ contracts/Payment.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Payment {
+
+//functionality:
+//Anybody deposit erc20 tokens (bUSD)
+//X withdraws deposited amount
+//owner can add more balances
+//create an event for every function
+
+//variables
+mapping(address => uint256) balances;
+
+//construct
+
+//functions
+
+}
