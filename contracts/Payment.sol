56f2683c8de409e8966c0dbcc680f2de267923e5
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2022-02-24 14:50:15 +0200

    mindmap

--- contracts/Payment.sol
+++ contracts/Payment.sol
@@ -5,7 +5,7 @@ import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Payment {
+contract Payment{
 
 //functionality:
 //Anybody deposit erc20 -> interface tokens (bUSD -> address)
@@ -15,19 +15,31 @@ contract Payment {
 
 //variables
 address public paymentToken = 0x4Fabb145d64652a948d72533023f6E7A623C7C53;
+address public owner;
 mapping(address => uint256) balances;
+mapping(address => mapping(address => uint256)) private allowances;
 
-//constructor
-// constructor (address _paymentToken) {
-//     paymentToken = _paymentToken;
-// }
-
-//functions
+constructor() {
+    owner = msg.sender;
+}
 
 //function to deposit
 //updates balaces and adds amount to address given
+    function transferTokens(address _to, uint256 _amount) external returns (bool) {
+        _to = address(this);
+        IERC20(paymentToken).transfer(_to, _amount);
+        return true;
+    }
+
 
 //function to withdraw
-//checks balances and then transfers erc20 to sender
+//checks balances and then transfers erc20 to sender (address that evokes function)
+//function will be connected to "Claim all" button in front-end
+    function claimTokens(address _from, address _to, uint256 _amount) external returns (bool) {
+        _from =  address(this);
+        IERC20(paymentToken).transferFrom(_from, _to, _amount);
+        return true;
+    }
+
 
 }
