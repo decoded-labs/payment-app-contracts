cf5113c37354bd05e5548c582b89788002cccd57
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2022-02-24 10:38:32 +0200

    Add license identifier

--- contracts/Payment.sol
+++ contracts/Payment.sol
@@ -3,21 +3,31 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Payment {
 
 //functionality:
-//Anybody deposit erc20 tokens (bUSD)
+//Anybody deposit erc20 -> interface tokens (bUSD -> address)
 //X withdraws deposited amount
-//owner can add more balances
+//Owner can add more balances(address -> allowance)
 //create an event for every function
 
 //variables
+address public paymentToken = 0x4Fabb145d64652a948d72533023f6E7A623C7C53;
 mapping(address => uint256) balances;
 
-//construct
+//constructor
+// constructor (address _paymentToken) {
+//     paymentToken = _paymentToken;
+// }
 
 //functions
+
+//function to deposit
+//updates balaces and adds amount to address given
+
+//function to withdraw
+//checks balances and then transfers erc20 to sender
 
 }
