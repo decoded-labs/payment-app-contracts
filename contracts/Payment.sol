e908ab8105a393770d7d7cb54c60dde0b368513c
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2022-02-28 11:57:11 +0200

    Update functions

--- contracts/Payment.sol
+++ contracts/Payment.sol
@@ -9,34 +9,33 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Payment is Ownable {
 
 //functionality:
-//Anybody deposit erc20 -> interface tokens (bUSD -> address)
-//X withdraws deposited amount
+//Owner deposit erc20 -> interface tokens (bUSD -> address)
+//X withdraws deposited amount (address must be in the balances mapping)
 //create an event for every function
 
 //variables
 address public paymentToken = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
+address public bonusToken;
 mapping(address => uint256) balances;
 
+    function addNewBalance(address _address, uint _balance, address _bonusTokenAddress, uint _bonusTokenAmount) public onlyOwner {
+        balances[_address] += _balance;
+        IERC20(paymentToken).transferFrom( msg.sender, address(this), _balance);
+        bonusToken = _bonusTokenAddress;
+        balances[_address] += _bonusTokenAmount;
+        IERC20(bonusToken).transferFrom( msg.sender, address(this), _bonusTokenAmount);
+    }
+    
 //function to withdraw
 //checks balances and then transfers erc20 to sender (address that evokes function)
 //function will be connected to "Claim all" button in front-end
+
     function claimTokens() external {
         uint _amount = balances[msg.sender];
         require(balances[msg.sender] > 0, "Cannot claim 0 tokens");
         IERC20(paymentToken).transfer( msg.sender, _amount);
         balances[msg.sender] -= _amount;
+        IERC20(bonusToken).transfer( msg.sender, _amount);
+        balances[msg.sender] -= _amount;
     }
-
-    function addNewBalance(address _address, uint _balance) public onlyOwner {
-        balances[_address] += _balance;
-        IERC20(paymentToken).transferFrom( msg.sender, address(this), _balance);
-    }
-
-    function addBonus(address _tokenAddress, uint _amount) public onlyOwner {
-        paymentToken = _tokenAddress;
-        balances[msg.sender] += _amount;
-        IERC20(paymentToken).transferFrom( msg.sender, address(this), _amount);
-
-    }
-
 }
