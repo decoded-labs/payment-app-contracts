e57c515c2104171c813b312ff19ec271820ac6fd
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2022-02-28 17:06:11 +0200

    add nonReentrant modifier

--- contracts/Payment.sol
+++ contracts/Payment.sol
@@ -5,8 +5,9 @@ import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Payment is Ownable {
+contract Payment is Ownable, ReentrancyGuard {
 
 //functionality:
 //Owner deposit erc20 -> interface tokens (bUSD -> address)
@@ -24,22 +25,35 @@ event Transfer(address indexed from, address indexed to, uint256 value);
     function addNewBalance(address _address, uint _balance, address _bonusTokenAddress, uint _bonusTokenAmount) public onlyOwner {
         balances[_address] += _balance;
         IERC20(paymentToken).transferFrom( msg.sender, address(this), _balance);
+
+        if (_bonusTokenAmount > 0) {
         bonusToken = _bonusTokenAddress;
         bonusBalances[_address] += _bonusTokenAmount;
         IERC20(bonusToken).transferFrom( msg.sender, address(this), _bonusTokenAmount);
+        }
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
     }
     
+    function bonusBalanceOf(address account) external view returns (uint256) {
+        return bonusBalances[account];
+    }
 //function to withdraw
 //checks balances and then transfers erc20 to sender (address that evokes function)
 //function will be connected to "Claim all" button in front-end
 
-    function claimTokens() external {
+    function claimTokens() external nonReentrant {
         uint _amount = balances[msg.sender];
         require(balances[msg.sender] > 0, "Cannot claim 0 tokens");
         IERC20(paymentToken).transfer( msg.sender, _amount);
         balances[msg.sender] -= _amount;
+
+        if (bonusBalances[msg.sender] > 0) {
         uint _bonusAmount = bonusBalances[msg.sender];
         IERC20(bonusToken).transfer( msg.sender, _bonusAmount);
         bonusBalances[msg.sender] -= _bonusAmount;
+        }
     }
 }
