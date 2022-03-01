333ec0ab8ac6bd86e0dc1ec40da567a5a41b8c02
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2022-03-01 15:24:03 +0200

    Delete approve function

--- contracts/Payment.sol
+++ contracts/Payment.sol
@@ -13,23 +13,10 @@ address public paymentToken = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
 address public bonusToken;
 mapping(address => uint256) balances;
 mapping(address => uint256) bonusBalances;
-mapping(address => mapping (address => uint256)) private allowed;
 
 event Transfer(address indexed from, address indexed to, uint256 value);
-event Approval(address owner, address spender, uint256 amount);
-
-    function approve(address _spender, uint256 _amount) public {
-        IERC20(paymentToken).approve(msg.sender, _amount);  
-        console.log("token approved");
-        allowed[msg.sender][_spender] = _amount;
-        console.log("token added to allowed mapping");
-        console.log(allowed[msg.sender][_spender]);
-        emit Approval(msg.sender, _spender, _amount);
-    }
 
     function addNewBalance(address _address, uint _balance, address _bonusTokenAddress, uint _bonusTokenAmount) public onlyOwner {
-
-
         balances[_address] += _balance;
         console.log("balance added");
 
--- test/payment-test.js
+++ test/payment-test.js
@@ -2,26 +2,6 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Payment", function () {
-  it("Should approve adress", async function () {
-    const Payment = await ethers.getContractFactory("Payment");
-    console.log("got Contract Factory");
-    const payment = await Payment.deploy();
-    await payment.deployed();
-    console.log("payment deployed");
-
-    const approveNewToken = await payment.approve("0xE13fC3A87F9890f6A7314f0789841c0E470ED08f", ethers.utils.parseEther("1"));
-    
-    console.log("address approved");
-
-    // wait until the transaction is mined
-    await approveNewToken.wait();
-    console.log("transaction is mined");
-
-    // expect(await payment.approveNewToken(allowed[msg.sender][_spender]).to.equal(_amount));
-  });
-});
-
-describe("Payment", function () {
   it("Should add new balance", async function () {
     const Payment = await ethers.getContractFactory("Payment");
     console.log("got Contract Factory");
