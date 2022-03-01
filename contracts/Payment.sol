6e106ece0ba693bb26887027c6f1e1efba96f481
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2022-03-01 14:00:41 +0200

    Add approve function

--- contracts/Payment.sol
+++ contracts/Payment.sol
@@ -9,29 +9,42 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Payment is Ownable, ReentrancyGuard {
 
-//functionality:
-//Owner deposit erc20 -> interface tokens (bUSD -> address)
-//X withdraws deposited amount (address must be in the balances mapping)
-//create an event for every function
-
-//variables
 address public paymentToken = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
 address public bonusToken;
 mapping(address => uint256) balances;
 mapping(address => uint256) bonusBalances;
+mapping(address => mapping (address => uint256)) private allowed;
 
 event Transfer(address indexed from, address indexed to, uint256 value);
+event Approval(address owner, address spender, uint256 amount);
+
+    function approve(address _spender, uint256 _amount) public {
+        IERC20(paymentToken).approve(msg.sender, _amount);  
+        console.log("token approved");
+        allowed[msg.sender][_spender] = _amount;
+        console.log("token added to allowed mapping");
+        console.log(allowed[msg.sender][_spender]);
+        emit Approval(msg.sender, _spender, _amount);
+    }
 
     function addNewBalance(address _address, uint _balance, address _bonusTokenAddress, uint _bonusTokenAmount) public onlyOwner {
+
+
         balances[_address] += _balance;
-        IERC20(paymentToken).transferFrom( msg.sender, address(this), _balance);
+        console.log("balance added");
+
+        IERC20(paymentToken).transfer(address(this), _balance);
+        console.log("tokens transfered");
+
 
         if (_bonusTokenAmount > 0) {
         bonusToken = _bonusTokenAddress;
+        IERC20(bonusToken).approve(_address, _bonusTokenAmount);
         bonusBalances[_address] += _bonusTokenAmount;
-        IERC20(bonusToken).transferFrom( msg.sender, address(this), _bonusTokenAmount);
+        IERC20(bonusToken).transferFrom(msg.sender, address(this), _bonusTokenAmount);
         }
     }
+
 
     function balanceOf(address account) external view returns (uint256) {
         return balances[account];
@@ -40,14 +53,11 @@ event Transfer(address indexed from, address indexed to, uint256 value);
     function bonusBalanceOf(address account) external view returns (uint256) {
         return bonusBalances[account];
     }
-//function to withdraw
-//checks balances and then transfers erc20 to sender (address that evokes function)
-//function will be connected to "Claim all" button in front-end
 
     function claimTokens() external nonReentrant {
         uint _amount = balances[msg.sender];
         require(balances[msg.sender] > 0, "Cannot claim 0 tokens");
-        IERC20(paymentToken).transfer( msg.sender, _amount);
+        IERC20(paymentToken).transfer(msg.sender, _amount);
         balances[msg.sender] -= _amount;
 
         if (bonusBalances[msg.sender] > 0) {
--- test/payment-test.js
+++ test/payment-test.js
@@ -2,6 +2,26 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Payment", function () {
+  it("Should approve adress", async function () {
+    const Payment = await ethers.getContractFactory("Payment");
+    console.log("got Contract Factory");
+    const payment = await Payment.deploy();
+    await payment.deployed();
+    console.log("payment deployed");
+
+    const approveNewToken = await payment.approve("0xE13fC3A87F9890f6A7314f0789841c0E470ED08f", ethers.utils.parseEther("1"));
+    
+    console.log("address approved");
+
+    // wait until the transaction is mined
+    await approveNewToken.wait();
+    console.log("transaction is mined");
+
+    // expect(await payment.approveNewToken(allowed[msg.sender][_spender]).to.equal(_amount));
+  });
+});
+
+describe("Payment", function () {
   it("Should add new balance", async function () {
     const Payment = await ethers.getContractFactory("Payment");
     console.log("got Contract Factory");
@@ -10,7 +30,7 @@ describe("Payment", function () {
     console.log("payment deployed");
 
     const setNewBalance = await payment.addNewBalance("0xE13fC3A87F9890f6A7314f0789841c0E470ED08f", ethers.utils.parseEther("1"), 
-    "0x6AA217312960A21aDbde1478DC8cBCf828110A67", ethers.utils.parseEther("100"));
+    "0x6AA217312960A21aDbde1478DC8cBCf828110A67", ethers.utils.parseEther("50"));
     
     console.log("new balance is set");
 
@@ -18,7 +38,6 @@ describe("Payment", function () {
     await setNewBalance.wait();
     console.log("transaction is mined");
 
-    expect(await payment.bonusBalanceOf("0xE13fC3A87F9890f6A7314f0789841c0E470ED08f")).to.equal(0);
+    expect(await payment.bonusBalanceOf("0xE13fC3A87F9890f6A7314f0789841c0E470ED08f")).to.equal(50);
   });
-  
 });
